14bf8f8817afcc0e9332fe17c7d258cd87564220
lxsang <xsang.le@gmail.com>
2020-12-08 23:23:59 +0100

    rename project

--- ros2/src/jarvis_core/setup.py
+++ ros2/src/jarvis_core/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'core = jarvis_core.core:main'
+            'base = jarvis_core.base:main'
         ],
     },
 )
