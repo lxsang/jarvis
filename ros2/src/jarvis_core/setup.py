bfe49ede51a27d7cc7466d67c38e4a94d5f0a133
Xuan Sang LE <xsang.le@gmail.com>
2020-12-11 23:37:38 +0100

    Update setup.py

--- ros2/src/jarvis_core/setup.py
+++ ros2/src/jarvis_core/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'base = jarvis_core.base:main'
+            'base = jarvis_core.jetty:main'
         ],
     },
 )
